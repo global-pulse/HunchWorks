a97febeec32fe8c90b3ec46cb690b72565e672bf
foxtana <leah@foxtana.com>
2011-06-26 19:07:56 -0700

    Re-factors the models file and dbs schema. This is a change for discussion; once we're agreed on the approach to take, I'll make any other changes required throughout the code to use the model structure and then merge to master.
    Items marked DISCUSS are, somewhat self-evidently, points where it would be helpful to get your feedback and input. It's *very* likely that we'll want to add some redundancy to this schema to make querying simpler, but I'd like to get to a clean, bare-bones schema and build up from that based on learnings from developing the app / tuning queries first.
    
    Changes:
    1. Moves all choices into a hunchworks_enums.py file, and makes most of them IntegerFields.
    2. Replaces most uses of varchar(1000) fields; by and large these are replaced with M2M tables. Chris - I'm happy to walk you through why long varchar fields in the way you were using them are a bad idea vs. use of relational semantics if that would be helpful?
    3. Consolidates various fields, e.g. languages, skill and expertise.
    4. Makes use of various Django special fields, e.g. URLField etc.
    5. Adds in stub i18n code to settings
    6. Adds docstrings throughout.
    7. Adds a whole lot of new tables, e.g. Role, Skill etc., to represent additional logical objects in the application.
    
    N.B. This change assumes that we're going to be using an RDBMS for application hosting; hence the heavy use of relational semantics etc.

---
+++ hunchworks/hunchworks_enums.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python2.7
+
+"""Enums used throughout Hunchworks."""
+
+__author__ = ('Leah',)
+__license__ = 'GPLv3'
+
+
+class Error(Exception):
+  pass
+
+
+class EnumException(Error):
+  pass
+
+
+class Enum(object):
+  """Class representing an enum in Python."""
+  MAP = {}
+  VALUE_LOOKUP = {}
+
+  @classmethod
+  def GetChoices(cls):
+    """Returns a tuple representing the enum.
+
+    Returns:
+      A nested tuple, where each child tuple contains:
+        (enum_value, user_facing_enum_value,)
+    """
+    enum_values = cls.GetEnumValues().items()
+    return [(x[1], cls.GetValue(x[0]),) for x in enum_values]
+
+  @classmethod
+  def GetValue(cls, lookup):
+    """Returns the user facing representation of an enum value.
+
+    Args:
+      lookup: The enum value to lookup.
+
+    Returns:
+      The user facing representation of the enum value.
+
+    Raises:
+      EnumException: Raised if the supplied value doesn't exist on the enum.
+    """
+    try:
+      assert(lookup in cls.GetEnumValues().keys())
+    except AssertionError:
+      raise EnumException(
+          'The supplied lookup value does not exist on the Enum')
+
+    try:
+      return cls.VALUE_LOOKUP[lookup]
+    except KeyError:
+      return lookup.title()
+
+  @classmethod
+  def GetEnumValues(cls):
+    """Get's the Enum values as a dictionary.
+
+    Returns:
+      A dictionary wrapping the Enum values.
+    """
+    if not cls.MAP:
+      # This is a bit of a hack - if the enum is defined in a module called
+      # directly from the command line, it also has a __module__ attribute,
+      # which the dummy object won't have.
+      standard_attrs = ['__module__'] + dir(type('dummy', (object,), {}))
+      class_attrs = inspect.getmembers(cls)
+      for item in class_attrs:
+        if (item[0] not in standard_attrs and
+            not inspect.isroutine(item[1]) and
+            not isinstance(item[1], (list, dict,))):
+          cls.MAP[item[0]] = item[1]
+
+    return cls.MAP
+
+
+class ConnectionStatus(Enum):
+  """Enum representing the possible statuses a user connection can take."""
+  BLOCKED = 0
+  FRIEND = 1
+
+
+class UserTitle(Enum):
+  """Enum representing the possible titles users can use."""
+  FIELD_LOOKUP = {
+      UserTitle.MR: 'Mr.',
+      UserTitle.MRS: 'Mrs.',
+      UserTitle.MS: 'Ms.',
+      }
+
+  MR = 0
+  MRS = 1
+  MS = 2
+
+
+class PrivacyLevel(Enum):
+  """Enum representing the various privacy levels available to users."""
+  HIDDEN = 0
+  CLOSED = 1
+  OPEN = 2
+
+
+class SkillLevel(Enum):
+  """Enum representing the various skill levels a user can have in a subject."""
+  EXPERT = 0
+  SKILLED = 1
+
+
+class HunchStatus(Enum):
+  """Enum representing the statuses a Hunch can take."""
+  CONFIRMED = 0
+  DENIED = 1
+  UNDETERMINED = 2
+
+
+class Group(Enum):
+  FIELD_LOOKUP = {
+      Group.AD_HOC: 'Ad-Hoc',
+      Group.ALUMNI: 'Alumni',
+      Group.COMPLEMENT: 'Complement',
+      Group.CORPORATE: 'Corporate',
+      Group.INTEREST: 'Interest',
+      Group.NON_PROFIT: 'Non-Profit',
+      }
+
+  AD_HOC = 0
+  ALUMNI = 1
+  COMPLEMENT = 2
+  CORPORATE = 3
+  INTEREST = 4
+  NON_PROFIT = 5
+
+
+class GroupPrivilege(Enum):
+  ADMIN = 0
+  MEMBER = 1
+
+
+ATTACHMENT_TYPES = (
+  ('Photo', 'Photo'),
+  ('Link', 'Link'),
+  ('Video', 'Video'),
+)
