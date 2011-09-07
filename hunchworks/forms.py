5c5c169dbbb730e8b26ce97dbc88f909634e0ef4
Adam Mckaig <adam.mckaig@gmail.com>
2011-09-07 14:27:47 -0400

    Remove huge license boilerplate from .py files.

--- hunchworks/forms.py
+++ hunchworks/forms.py
@@ -1,14 +1,4 @@
 #!/usr/bin/env python
-# Forms used in the project
-# Author: Auto created by Texas
-# Date: 2011-06-20
-# License:  This  program  is  free  software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as published by
-# the  Free Software Foundation; either version 3 of the License, or (at your
-# option)  any later version. This program is distributed in the hope that it
-# will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-# Public License for more details.
 
 import models
 import custom_fields
--- hunchworks/tests.py
+++ hunchworks/tests.py
@@ -1,14 +1,4 @@
 #!/usr/bin/env python
-# Base views files for the HunchWorks application.
-# Author: Leah
-# Date: 2011-6-16
-# License:  This  program  is  free  software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as published by
-# the  Free Software Foundation; either version 3 of the License, or (at your
-# option)  any later version. This program is distributed in the hope that it
-# will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-# Public License for more details.
 
 """
 This file demonstrates writing tests using the unittest module. These will pass
