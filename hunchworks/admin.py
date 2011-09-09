9b88ef3895f0942efc59613a67f18d983f1c4e65
Adam Mckaig <adam.mckaig@gmail.com>
2011-09-09 11:42:51 -0400

    Add all models to admin.

--- hunchworks/admin.py
+++ hunchworks/admin.py
@@ -1,17 +1,26 @@
-import models
+#!/usr/bin/env python
+
+from hunchworks.models import *
 from django.contrib import admin
 
-### EXAMPLE ###
-# class AuthorAdmin(admin.ModelAdmin):
-#     list_display = ('first_name', 'last_name', 'email')
-#     search_fields = ('first_name', 'last_name')
- 
-# admin.site.register(Publisher)
-# admin.site.register(Author, AuthorAdmin)
-
-
-admin.site.register(models.UserProfile)
-admin.site.register(models.Hunch)
-admin.site.register(models.Tag)
-admin.site.register(models.Skill)
-admin.site.register(models.SkillConnections)
+admin.site.register(Album)
+admin.site.register(Attachment)
+admin.site.register(AlbumAttachments)
+admin.site.register(Class)
+admin.site.register(Hunch)
+admin.site.register(Evidence)
+admin.site.register(EvidenceAlbums)
+admin.site.register(EvidenceAttachments)
+admin.site.register(Group)
+admin.site.register(GroupConnections)
+admin.site.register(InvitedUser)
+admin.site.register(HunchConnections)
+admin.site.register(LocationInterests)
+admin.site.register(Organization)
+admin.site.register(Role)
+admin.site.register(Skill)
+admin.site.register(SkillConnections)
+admin.site.register(UserRoles)
+admin.site.register(Tag)
+admin.site.register(TagConnections)
+admin.site.register(UserConnections)
