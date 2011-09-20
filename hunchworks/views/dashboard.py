a55fd7e7ba1ba23ebfec4d8c6abd351acbf82a74
Alex Gaudio <adgaudio@gmail.com>
2011-09-19 20:39:03 -0400

    Recent (Confirmed and Unconfirmed) Hunches show up on home page with time since last modified

--- hunchworks/views/dashboard.py
+++ hunchworks/views/dashboard.py
@@ -13,8 +13,17 @@ def index(request):
 @login_required
 def home(request):
   user_id = request.user.pk
-  recent_hunches = models.Hunch.objects.filter(privacy=hunchworks_enums.PrivacyLevel.OPEN).order_by("-time_modified")[:5]
+  recent_hunches = \
+          models.Hunch.objects.filter(
+            privacy=hunchworks_enums.PrivacyLevel.OPEN, status=2
+            ).order_by("-time_modified")[:5]
+  confirmed_hunches = \
+          models.Hunch.objects.filter(
+                  privacy=hunchworks_enums.PrivacyLevel.OPEN, status__in=(0,1)
+                  ).order_by("-time_modified")[:5]
   suggested_groups = models.Group.objects.all()
   context = RequestContext(request)
-  context.update({'recent_hunches': recent_hunches, 'suggested_groups': suggested_groups })
+  context.update({'recent_hunches': recent_hunches,
+      'confirmed_hunches': confirmed_hunches,
+      'suggested_groups': suggested_groups })
   return render_to_response('home.html', context)
