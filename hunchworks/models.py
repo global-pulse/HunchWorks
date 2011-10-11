b317009b3d1b8f622494a497907a4cc64ca0cb10
Alex Gaudio <adgaudio@gmail.com>
2011-10-10 20:26:53 -0400

    Removed screen name UserProfile since its not in code base.

--- hunchworks/models.py
+++ hunchworks/models.py
@@ -35,7 +35,6 @@ class UserProfile(models.Model):
   skype_name = models.CharField(max_length=30, blank=True)
   website = models.CharField(max_length=100, blank=True)
   profile_picture = models.ImageField(upload_to="profile_images", blank=True)
-  screen_name = models.CharField(max_length=45, blank=True)
   messenger_service = models.IntegerField(null=True, blank=True, choices=hunchworks_enums.MessangerServices.GetChoices(), default=0)
   translation_language = models.ForeignKey('TranslationLanguage', default=0)
 
