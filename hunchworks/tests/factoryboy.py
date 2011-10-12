f60c5a54fa28fc1610ff88f94796e305f9d7fc4f
Alex Gaudio <adgaudio@gmail.com>
2011-10-12 01:42:34 -0400

    still in progress.

--- hunchworks/tests/factoryboy.py
+++ hunchworks/tests/factoryboy.py
@@ -12,9 +12,20 @@ class HelperMixin(object):
         model = getattr(models, model_name)
         return [x for x in model.objects.all() if random.random() > percent]
 
+    def _getpks(model_name):
+        """Get flattened list of primary keys"""
+        model = getattr(models, model_name)
+        pks = model.objects.values_list('pk', flat=True)
+        return pks, model
+
     def randInstance(model_name):
         """Return randomly selected instance of possible models"""
-        pass # for things like user, translation_language, etc.
+
+        pks, model = self._getpks(model_name)
+        return model.objects.get(pk=random.choice(pks))
+
+    #def newInstance(model_name):
+        #""""return model.objects.get(pk=sorted(pks)[-1])
 
 class UserProfileFactory(factory.Factory, HelperMixin):
     #user = factory.Sequence(lambda n: str(int(n)+1))
@@ -24,25 +35,26 @@ class UserProfileFactory(factory.Factory, HelperMixin):
     privacy = random.choice(enums.PrivacyLevel.GetChoices())[0]
 
     #blank = True for all below
-    bio_text = "Soon to be markov text"
-    phone = ''.join([str(random.randint(0,9))
-                    for x in range(random.choice([7,10,11,13,20]))])
-    skype_name = "%s_onskype" % name
-    website = "%s%s.%s" % (random.choice(['www','', 'http://', 'http://www.']),
-                    name,
-                    random.choice(['.com', '.org', '.me', '.uk', '.it']))
-    #profile_picture = models.ImageField(upload_to="profile_images", blank=True)
-    messenger_service = random.choice(enums.MessangerServices.GetChoices())[0]
-    translation_language = models.TranslationLanguage.objects.get(pk=random.choice(
-                    models.TranslationLanguage.objects.values_list('pk', flat=True)))
-    #invitation = random.choice(models.Invitation.objects.values_list('pk', flat=True))
+    #bio_text = "Soon to be markov text"
+    #phone = ''.join([str(random.randint(0,9))
+                    #for x in range(random.choice([7,10,11,13,20]))])
+    #skype_name = "%s_onskype" % name
+    #website = "%s%s.%s" % (random.choice(['www','', 'http://', 'http://www.']),
+                    #name,
+                    #random.choice(['.com', '.org', '.me', '.uk', '.it']))
+    ##profile_picture = models.ImageField(upload_to="profile_images", blank=True)
+    #messenger_service = random.choice(enums.MessangerServices.GetChoices())[0]
+    #translation_language = models.TranslationLanguage.objects.get(pk=random.choice(
+                    #models.TranslationLanguage.objects.values_list('pk', flat=True)))
+    ##invitation = random.choice(models.Invitation.objects.values_list('pk', flat=True))
 
-    connections = handlem2m('UserProfile', .5)
-    roles = handlem2m('Role', .7)
-    location_interests = handlem2m('Location', .7)
-    skills = handlem2m('Skill', .7)
-    languages = handlem2m('Language', .7)
+    #connections = handlem2m('UserProfile', .5)
+    #roles = handlem2m('Role', .7)
+    #location_interests = handlem2m('Location', .7)
+    #skills = handlem2m('Skill', .7)
+    #languages = handlem2m('Language', .7)
 
-    qualifications = handlem2m('Education', .7)
-    courses = handlem2m('Course', .7)
+    #qualifications = handlem2m('Education', .7)
+    #courses = handlem2m('Course', .7)
+
 
