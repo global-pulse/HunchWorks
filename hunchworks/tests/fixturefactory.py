6dd78267e0f78c099e29a177d761e0966f593ad9
Alex Gaudio <adgaudio@gmail.com>
2011-10-14 00:32:58 -0400

    fixturefactory latest

--- hunchworks/tests/fixturefactory.py
+++ hunchworks/tests/fixturefactory.py
@@ -1,24 +1,10 @@
 import random
 
-from hunchworks.models import UserProfile, TranslationLanguage, Invitation
-from django.contrib.auth.models import User
-from hunchworks import hunchworks_enums as enums
-
-class FactoryMixin(object):
-    """Base Class for creating django objects
-
-    Child classes must adhere to the following template:
-
-    class Child(FactoryMixin):
-        model = django.contrib.auth.models.User
-        def getparams(self): return {}
-
-    *model is the class we are mass producing
-    *getparams: see FactoryMixin.getparams.__doc__
-    """
+class BaseFactory(object):
+    """Base Class for creating django objects """
 
     def getparams(cls):
-        """Template method: must be defined by child class.
+        """Template method: must be overridden by child class.
         Return dict of params that get sent to self.create()
 
         Example logic:
@@ -30,7 +16,7 @@ class FactoryMixin(object):
         return locals()
         """
         raise NotImplementedError(
-                'You cannot directly instantiate FactoryMixin '
+                'You cannot directly instantiate BaseFactory '
                 'or call this method directly')
 
     def __init__(self, save_to_db=True):
@@ -60,7 +46,8 @@ class FactoryMixin(object):
             inst.save()
         return inst
 
-
+class FactoryMixin(object):
+    """Useful/Necessary methods for Fixture Factories"""
     def _getmodel(self, model=None):
         if model == None:
             return self.model
@@ -99,56 +86,4 @@ class FactoryMixin(object):
     #def newInstance(self, model=None):
         #""""return self.model.objects.get(pk=sorted(pks)[-1])
 
-class UserFactory(FactoryMixin):
-    model = User
 
-    def getparams(self):
-        """ Define parameters to create a new object with.
-        Return dict"""
-
-        pk = self.getUnusedPk()
-        username = 'markov_%s' % pk
-        password = username
-
-        return locals()
-
-class UserProfileFactory(FactoryMixin):
-    model = UserProfile
-    def phonenumber(self):
-        return  ''.join([str(random.randint(0,9))
-                for x in range(random.choice([7,10,11,13,20]))])
-    def website(self, subdomain):
-        return  "%s%s%s" %
-                    (random.choice(['www.','', 'http://', 'http://www.']),
-                     subdomain,
-                     random.choice(['.com', '.org', '.me', '.uk', '.it']))
-
-    def getparams(self):
-        user = UserFactory().last_obj_created
-        pk = user.pk
-        title = random.choice(enums.UserTitle.GetChoices())[0]
-        email = '%s@testhunchworks.com' % (user.username)
-        privacy = random.choice(enums.PrivacyLevel.GetChoices())[0]
-
-        ###blank = True for all below
-        bio_text = "Soon to be markov text"
-        phone = self.phonenumber()
-        skype_name = "%s_onskype" % user.username
-        website = self.website(user.username)
-        #profile_picture = models.ImageField(upload_to="profile_images", blank=True)
-        messenger_service = random.choice(enums.MessangerServices.GetChoices())[0]
-        translation_language = self.getRandInst(TranslationLanguage)
-        #invitation = self.getRandInst(Invitation)
-
-        #NO SUPPORT FOR MANY TO MANY YET. need to create multiple Connection,
-        # ALL BELOW ARE MANY TO MANY
-        #connections = ??? # don't know how to do connections
-        #roles = Role
-        #location_interests = Location
-        #skills = Skill
-        #languages = Language
-
-        #qualifications = Education
-        #courses = Course
-
-        return locals()
---
+++ hunchworks/tests/fixtures.py
@@ -0,0 +1,80 @@
+from fixturefactory import BaseFactory, FactoryMixin
+
+import random
+
+from hunchworks.models import UserProfile, Connection, TranslationLanguage, Invitation
+from django.contrib.auth.models import User
+from hunchworks import hunchworks_enums as enums
+
+
+class ConnectionFactory(BaseFactory, FactoryMixin):
+    model = Connection
+
+    def __init__(self, uid1, uid2, *args, **kwargs):
+        self.uid1 = uid1
+        self.uid2 = uid2
+        super(ConnectionFactory, self).__init__(*args, **kwargs)
+
+    def getparams(self):
+        status = random.choice(enums.ConnectionStatus.GetChoices())[0]
+        user_profile_id = self.uid1
+        other_user_profile_id = self.uid2
+        return locals()
+
+class UserFactory(BaseFactory, FactoryMixin):
+    model = User
+
+    def getparams(self):
+        """ Define parameters to create a new object with.
+        Return dict"""
+
+        pk = self.getUnusedPk()
+        username = 'markov_%s' % pk
+        password = username
+
+        return locals()
+
+class UserProfileFactory(BaseFactory, FactoryMixin):
+    model = UserProfile
+
+    def getparams(self):
+        user = UserFactory().last_obj_created
+        pk = user.pk
+        title = random.choice(enums.UserTitle.GetChoices())[0]
+        email = '%s@testhunchworks.com' % (user.username)
+        privacy = random.choice(enums.PrivacyLevel.GetChoices())[0]
+
+        ###blank = True for all below
+        bio_text = "Soon to be markov text"
+        phone = self.phonenumber()
+        skype_name = "%s_onskype" % user.username
+        website = self.website(user.username)
+        #profile_picture = models.ImageField(upload_to="profile_images", blank=True)
+        messenger_service = random.choice(enums.MessangerServices.GetChoices())[0]
+        translation_language = self.getRandInst(TranslationLanguage)
+        #invitation = self.getRandInst(Invitation)
+
+        #SUPPORT FOR MANY TO MANY: you need to create model objects for each one-to-one
+        # in the many-to-many relationship.
+        # ALL BELOW ARE MANY TO MANY
+        #connections => Connection
+        ConnectionFactory(pk, self.getRandInst(model=UserProfile).pk)
+        #roles = Role
+        #location_interests = Location
+        #skills = Skill
+        #languages = Language
+
+        #qualifications = Education
+        #courses = Course
+
+        return locals()
+
+    def phonenumber(self):
+        return  ''.join([str(random.randint(0,9))
+                for x in range(random.choice([7,10,11,13,20]))])
+    def website(self, subdomain):
+        return  "%s%s%s" % (
+                    random.choice(['www.','', 'http://', 'http://www.']),
+                    subdomain,
+                    random.choice(['.com', '.org', '.me', '.uk', '.it']))
+
