4bfd0dddb1fbce966823709ab8310ff49a32d776
Adam Mckaig <adam.mckaig@gmail.com>
2011-10-18 17:17:24 -0400

    Dump logs to the console.
    I have no idea why the default Django configuration is to send an email
    to the site admins, but not output 500 errors to console nor file. This
    does that.

--- settings.py
+++ settings.py
@@ -148,28 +148,25 @@ if DEBUG:
     'django.contrib.messages',
     'django.contrib.admin')
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
+# Dump logs to the console, for now.
 LOGGING = {
-  'version': 1,
-  'disable_existing_loggers': False,
-  'handlers': {
-    'mail_admins': {
-      'level': 'ERROR',
-      'class': 'django.utils.log.AdminEmailHandler'
+  "version": 1,
+
+  "handlers": {
+    "console": {
+      "level": "INFO",
+      "class": "logging.StreamHandler"
     }
   },
-  'loggers': {
-    'django.request': {
-      'handlers': ['mail_admins'],
-      'level': 'ERROR',
-      'propagate': True,
+
+  "loggers": {
+    "django": {
+      "handlers": ["console"],
+      "level": "INFO"
     },
   }
 }
+
 
 AUTH_PROFILE_MODULE = 'hunchworks.UserProfile'
 
