e3fac87368c0701ffaf93989ec54323c2ec8733e
Adam Mckaig <adam.mckaig@gmail.com>
2011-10-18 16:17:34 -0400

    Tidy up dashboard using new partials.
    This is not a good dashboard. I'm just tidying it up using the data
    which is already there, because it's been busted for far too long.

--- hunchworks/views/dashboard.py
+++ hunchworks/views/dashboard.py
@@ -12,18 +12,18 @@ def dashboard(req):
   recent_hunches = \
           models.Hunch.objects.filter(
             privacy=hunchworks_enums.PrivacyLevel.OPEN, status=2
-            ).order_by("-time_modified")[:5]
+            ).order_by("-time_modified")[:3]
   confirmed_hunches = \
           models.Hunch.objects.filter(
                   privacy=hunchworks_enums.PrivacyLevel.OPEN, status__in=(0,1)
-                  ).order_by("-time_modified")[:5]
+                  ).order_by("-time_modified")[:3]
   suggested_groups = models.Group.objects.exclude(pk__in=req.user.get_profile().group_set.all())
   suggested_people = models.UserProfile.objects.exclude(pk__in=req.user.get_profile().connections.all()).exclude(pk=req.user.get_profile().pk)
   context = RequestContext(req)
   context.update({'recent_hunches': recent_hunches,
       'confirmed_hunches': confirmed_hunches,
-      'suggested_groups': suggested_groups[:5],
-      'suggested_people': suggested_people[:5] })
+      'suggested_groups': suggested_groups[:3],
+      'suggested_people': suggested_people[:3] })
   return render_to_response('dashboard/home.html', context)
 
 
