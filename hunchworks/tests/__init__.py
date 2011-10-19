9f8e1327506046495f7543e704380c9031853628
Chris Blumberg <Bluestreak2k5@yahoo.com>
2011-10-19 18:36:09 -0400

    Added tests for Auth views closes #62

--- hunchworks/tests/__init__.py
+++ hunchworks/tests/__init__.py
@@ -4,3 +4,4 @@ from hunchworks.tests.group import *
 from hunchworks.tests.hunch import *
 from hunchworks.tests.evidence import *
 from hunchworks.tests.connection import *
+from hunchworks.tests.auth import *
---
+++ hunchworks/tests/auth.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from hunchworks.tests.helpers import TestHelpers
+from django.test import TestCase
+
+
+class AuthViewsTest(TestCase, TestHelpers):
+  fixtures = ("test_users",)
+
+  def test_login(self):
+    resp = self.get("login")
+    self.assertTemplateUsed(resp, "auth/login.html")
+    
+    post_resp = self.submit_form(resp, {
+        "username": "this shouldn't work",
+        "password": ""
+      })
+      
+    self.assertTemplateUsed(post_resp, "auth/login.html")
+    
+    post_resp2 = self.submit_form(resp, {
+        "username": "one",
+        "password": "one"
+      })
+
+    self.assertRedirects(post_resp2, "/")
+
+
+  def test_logout(self):
+    get_resp = self.get("logout")
+    self.assertRedirects(get_resp, "login")
+
+  def test_signup(self):
+    get_resp = self.get("signup")
+
+    post_resp = self.submit_form(get_resp, {
+        "username": "this shouldn't work",
+        "password1": "",
+        "password2": ""
+      })
+
+    self.assertTemplateUsed(post_resp, "auth/signup.html")
+
+    post_resp2 = self.submit_form(get_resp, {
+        "username": "four",
+        "password1": "four",
+        "password2": "four"
+      })
+
+    self.assertRedirects(post_resp2, "/")
