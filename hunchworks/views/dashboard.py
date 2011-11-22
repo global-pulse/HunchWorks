b8dab31884c6a4bffc7958272b6ed5b80ba9ceb3
Adam Mckaig <adam.mckaig@gmail.com>
2011-11-22 14:35:37 -0500

    Show recent events on the dashboard.

--- hunchworks/views/dashboard.py
+++ hunchworks/views/dashboard.py
@@ -7,21 +7,14 @@ from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
 
+def _render(req, template, more_context):
+  return render_to_response(
+    "dashboard/" + template +".html",
+    RequestContext(req, more_context))
+
+
 @login_required
 def dashboard(req):
-  recent_hunches = \
-          models.Hunch.objects.filter(
-            privacy=hunchworks_enums.PrivacyLevel.OPEN,
-            ).order_by("-time_modified")[:3]
-  confirmed_hunches = \
-          models.Hunch.objects.filter(
-                  privacy=hunchworks_enums.PrivacyLevel.OPEN,
-                  ).order_by("-time_modified")[:3]
-  suggested_groups = models.Group.objects.exclude(pk__in=req.user.get_profile().group_set.all())
-  suggested_people = models.UserProfile.objects.exclude(pk__in=req.user.get_profile().connections.all()).exclude(pk=req.user.get_profile().pk)
-  context = RequestContext(req)
-  context.update({'recent_hunches': recent_hunches,
-      'confirmed_hunches': confirmed_hunches,
-      'suggested_groups': suggested_groups[:3],
-      'suggested_people': suggested_people[:3] })
-  return render_to_response('dashboard/home.html', context)
+  return _render(req, "home", {
+    "events": req.user.get_profile().events()
+  })
