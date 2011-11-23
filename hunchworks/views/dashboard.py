7c2c90b1f749dda461957d27206b3fa34b6e3c4f
Adam Mckaig <adam.mckaig@gmail.com>
2011-11-22 19:11:05 -0500

    Tidy up dashboard.

--- hunchworks/views/dashboard.py
+++ hunchworks/views/dashboard.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from hunchworks import models, hunchworks_enums
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from hunchworks.utils.pagination import paginated
+from hunchworks import models, hunchworks_enums
 
 
 def _render(req, template, more_context):
@@ -15,6 +16,10 @@ def _render(req, template, more_context):
 
 @login_required
 def dashboard(req):
+  events = paginated(req, req.user.get_profile().events(), 20)
+  hunches = paginated(req, req.user.get_profile().hunch_set.all(), 10)
+
   return _render(req, "home", {
-    "events": req.user.get_profile().events()
+    "events": events,
+    "hunches": hunches
   })
