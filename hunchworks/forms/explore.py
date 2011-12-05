7d175dfacda56fffdf04638749086f91976bb7cd
Chris Blumberg <Bluestreak2k5@yahoo.com>
2011-12-05 16:48:23 -0500

    Removed uneeded help text on Explore form.

--- hunchworks/forms/explore.py
+++ hunchworks/forms/explore.py
@@ -8,7 +8,4 @@ class ExploreWorldBankForm(forms.Form):
     help_text="There are roughly 5000 indicators, you must type 3 letters to see a list of matching indicators. " +
     "You are also only limited to 1 indicator at a time; to see another indicator, remove the first indicator to search again." )
   country = forms.CharField(required=False, 
-    help_text="You are limited to entering 4 countries you want to see, however you can see all countries " +
-      "for this indicatior by leaving the country field blank. "
-      "Please be aware that Continents and Regions cannot be saved for visualizations " +
-      "only individual countries can. If you try to visualize regions or continents, you will get an error.")
+    help_text="You can see all countries for this indicatior by leaving the country field blank. ")
